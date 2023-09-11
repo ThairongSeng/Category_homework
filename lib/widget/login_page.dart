134cb90291d03ca1946d852b3764f72379e9d9b6
ThairongSeng <seng.thairong@gmail.com>
2023-09-11 16:07:07 +0700

    category page

---
+++ lib/widget/login_page.dart
@@ -0,0 +1,14 @@
+import 'package:category_homework/widget/register_page.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+
+    );
+  }
+}
